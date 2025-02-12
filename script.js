b9c6ddba4ae2dbaa7e370b40d133a84dc0870283
Rio <aryont52@gmail.com>
2025-02-13 06:35:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 120,
+            "density": {
+                "enable": true,
+                "value_area": 1000
+            }
+        },
+        "color": {
+            "value": "#FFD700"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 6
+            }
+        },
+        "opacity": {
+            "value": 0.7,
+            "random": false,
+            "anim": {
+                "enable": true,
+                "speed": 1.5,
+                "opacity_min": 0.3,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 5,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 4,
+                "size_min": 1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": false
+        },
+        "move": {
+            "enable": true,
+            "speed": 3,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false
+        }
+    },
+    "retina_detect": true
+});
